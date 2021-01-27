899eb206cab2a1e3906955b38b27153041e51c74
minymanz <afifhan95@gmail.com>
2021-01-27 19:11:02 +0100

    - Create a dummy function to see if FFT works right, and it is working perfectly fine - Tried to implement dummy function to fineup function, somehow return invalid CUFFT.

--- my_phase-retrieval/example.py
+++ my_phase-retrieval/example.py
@@ -2,32 +2,40 @@ import cuPhaseRet #c++ pybind phase retrieval
 import numpy as np
 import imageio
 import matplotlib.pyplot as plt
-# from phase_retrieval import fienup_phase_retrieval
-# from time import perf_counter 
+from phase_retrieval import fienup_phase_retrieval
+from time import perf_counter 
 
 
 np.random.seed(1)
-image = imageio.imread('a.png', as_gray=True)
+image = imageio.imread('test.png', as_gray=True)
 magnitudes = np.abs(np.fft.fft2(image))
 
-result = cuPhaseRet.fienup_phase_retrieval(magnitudes, 500, False, "hybrid", 0.8)
+result = cuPhaseRet.fienup_phase_retrieval(magnitudes, 2, False, "hybrid", 0.8)
 
-print(magnitudes.shape)
-print(result.shape)
-# result = np.reshape(result, magnitudes.shape)
+# cuPhaseRet.test_fft(magnitudes)
+
+
 
 # t1_start = perf_counter()
 
-# result = fienup_phase_retrieval(magnitudes, steps=500,
-#                                 verbose=False)
+result2 = fienup_phase_retrieval(magnitudes, steps=2, verbose=False)
+
 # t1_stop = perf_counter() 
 # print("Elapsed time during the whole program in seconds:", t1_stop-t1_start) 
 
-plt.show()
-plt.subplot(121)
-plt.imshow(image, cmap='gray')
-plt.title('Image')
-plt.subplot(122)
-plt.imshow(result, cmap='gray')
-plt.title('Reconstruction')
-plt.show()
+# print(magnitudes.shape)
+# print(result.shape)
+
+for i in range(2):
+    for j in range(2):
+        print(result[i][j]," ", result2[i][j])
+
+
+# plt.show()
+# plt.subplot(121)
+# plt.imshow(magnitudes, cmap='gray')
+# plt.title('Image')
+# plt.subplot(122)
+# plt.imshow(result, cmap='gray')
+# plt.title('Reconstruction')
+# plt.show()
--- my_phase-retrieval/phase_algo.hpp
+++ my_phase-retrieval/phase_algo.hpp
@@ -1,6 +1,7 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <cufft.h>
+#include <cuComplex.h>
 #include <cmath>
 #include <cstdio>
 #include <time.h>
@@ -9,20 +10,35 @@
 #include <string>
 #include <chrono>
 
-#define CUDA_CHECK(cmd) {cudaError_t error = cmd; if(error!=cudaSuccess){printf("<%s>:%i ",__FILE__,__LINE__); printf("[CUDA] Error: %s\n", cudaGetErrorString(error));}}
+#define PI 3.1415926535897932384626433
+#define CUDA_CHECK(call) {cudaError_t error = call; if(error!=cudaSuccess){printf("<%s>:%i ",__FILE__,__LINE__); printf("[CUDA] Error: %s\n", cudaGetErrorString(error));}}
 using namespace std;
 
 namespace py = pybind11;
 
+//test, still not used
+__device__ cuDoubleComplex my_complex_exp (cuDoubleComplex arg)
+{
+   cuDoubleComplex res;
+   float s, c;
+   float e = expf(arg.x);
+   sincosf(arg.y, &s, &c);
+   res.x = c * e;
+   res.y = s * e;
+   return res;
+}
+
 __global__ void get_real(cufftDoubleComplex *temp_y, double *y)
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    // int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int idx = blockIdx.x;
     y[idx] = temp_y[idx].x;
 }
 
 __global__ void get_complex(double *image_x, cufftDoubleComplex *image_x_comp)
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    // int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int idx = blockIdx.x;
     image_x_comp[idx].x = image_x[idx];
     image_x_comp[idx].y = 0;
 }
@@ -30,7 +46,8 @@ __global__ void get_complex(double *image_x, cufftDoubleComplex *image_x_comp)
 
 __global__ void process_arrays(double *mask, double *y, double *image_x, double *image_x_p, double beta, int mode, int iter)
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    // int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int idx = blockIdx.x;
     bool logical_not_mask;
     bool y_less_than_zero;
     bool logical_and;
@@ -91,8 +108,8 @@ py::array_t<double> fienup_phase_retrieval(py::array_t<double> mag, int steps, b
     else if(mode.compare("output-output") == 0) int_mode = 3;
 
     double *ptrMag = (double *) bufMag.ptr; //magnitude 1D
-    size_t X = bufMag.shape[0]; //width of magnitude
-    size_t Y = bufMag.shape[1]; //heght of magnitude
+    size_t X = bufMag.shape[1]; //width of magnitude
+    size_t Y = bufMag.shape[0]; //height of magnitude
     
     //alternative fot saving mag size, prevent warning while using CUFFT 
     //( warning C4267: 'argument': conversion from 'size_t' to 'int', possible loss of data)"
@@ -100,14 +117,13 @@ py::array_t<double> fienup_phase_retrieval(py::array_t<double> mag, int steps, b
     int size_x = static_cast<int>(X); 
     int size_y = static_cast<int>(Y);
     int dimension = size_x*size_y;
+    complex<double> complex1i(0, 1);
 
     //allocating arrays, all arrays bellow are 1D representation of 2D array
     double *mask = new double[dimension]; //mask array, same size as magnitude
     double *image_x = new double[dimension]; //initial image x, same size as magnitude
     double *image_x_p = new double[dimension]; //previous image for steps
-    double *y = new double[dimension]; //store inverse fourier transform (real number)
     complex<double> *y_hat = new complex<double>[dimension]; //sample random phase
-    complex<double> *temp_y =  new complex<double>[dimension]; //temporary complex version of y after CUFFT, before copying the real number to y
     complex<double> *x_hat = new complex<double>[dimension];
 
     auto begin = chrono::high_resolution_clock::now();
@@ -116,34 +132,33 @@ py::array_t<double> fienup_phase_retrieval(py::array_t<double> mag, int steps, b
     fill_n(&mask[0], dimension, 1.0); //fill mask with 1.0
     fill_n(&image_x[0], dimension, 0.0); //fill image_x with 0.0 for initial data
     fill_n(&image_x_p[0], dimension, 0.0); //fill image_x_p with 0.0 for initial data
-    fill_n(&y[0], dimension, 0.0); //store inverse fourier transform (real number)
     
     //this is not natively support in CUDA, still finding solution
     for (int i = 0; i < dimension; i++)
     {
         double rand_num = (double) rand()/RAND_MAX;
-        y_hat[i] = ptrMag[i]*exp(1i*2.0*3.14*rand_num); //random phase initial value
+        y_hat[i] = ptrMag[i]*exp(complex1i*2.0*PI*rand_num); //random phase initial value
     }
 
     double *y_dev_res;
-    cufftHandle plan;
-    cufftDoubleComplex *y_dev_start, *temp_y_dev;
+    cufftHandle plan2d;
+    cufftDoubleComplex *y_dev_start;
     CUDA_CHECK(cudaMalloc((void **) &y_dev_start, dimension * sizeof(cufftDoubleComplex)));
-    CUDA_CHECK(cudaMalloc((void **) &temp_y_dev, dimension * sizeof(cufftDoubleComplex)));
-    CUDA_CHECK(cudaMalloc(&y_dev_res, dimension * sizeof(double)));
+    CUDA_CHECK(cudaMalloc((void **) &y_dev_res, dimension * sizeof(double)));
 
     double *mask_dev, *image_x_device, *image_x_p_device;
-    CUDA_CHECK(cudaMalloc(&mask_dev, dimension * sizeof(double)));
-    CUDA_CHECK(cudaMalloc(&image_x_device, dimension * sizeof(double)));
-    CUDA_CHECK(cudaMalloc(&image_x_p_device, dimension * sizeof(double)));
+    CUDA_CHECK(cudaMalloc((void **) &mask_dev, dimension * sizeof(double)));
+    CUDA_CHECK(cudaMalloc((void **) &image_x_device, dimension * sizeof(double)));
+    CUDA_CHECK(cudaMalloc((void **) &image_x_p_device, dimension * sizeof(double)));
 
-    cufftDoubleComplex *image_x_dev_comp, *image_x_dev_res;
+    cufftDoubleComplex *image_x_dev_comp;
     CUDA_CHECK(cudaMalloc((void **) &image_x_dev_comp, dimension * sizeof(cufftDoubleComplex)));
-    CUDA_CHECK(cudaMalloc((void **) &image_x_dev_res, dimension * sizeof(cufftDoubleComplex)));
 
     CUDA_CHECK(cudaMemcpy(mask_dev, mask, dimension * sizeof(double), cudaMemcpyHostToDevice));
     CUDA_CHECK(cudaMemcpy(image_x_device, image_x, dimension * sizeof(double), cudaMemcpyHostToDevice));
     CUDA_CHECK(cudaMemcpy(image_x_p_device, image_x_p, dimension * sizeof(double), cudaMemcpyHostToDevice));
+
+    cufftResult fftresult;
 
     //iteration with number of steps------------------------------------------------------------------------------------------------------
     for(int iter = 0; iter < steps; iter++)
@@ -152,27 +167,31 @@ py::array_t<double> fienup_phase_retrieval(py::array_t<double> mag, int steps, b
 
         //create cufft plan
         //use Z2Z for complex double to complex double, use Z2Z because Z2D has no inverse option
-        cufftPlan2d(&plan, size_x, size_y, CUFFT_Z2Z);
-        cufftExecZ2Z(plan, y_dev_start, temp_y_dev, CUFFT_INVERSE);
+        fftresult = cufftPlan2d(&plan2d, dimension, dimension, CUFFT_Z2Z); //why is this plan invalid????
+        if(fftresult != CUFFT_SUCCESS) cout<<iter<<"\t"<<fftresult<<endl;
+
+        fftresult = cufftExecZ2Z(plan2d, y_dev_start, y_dev_start, CUFFT_INVERSE);
+        if(fftresult != CUFFT_SUCCESS) cout<<iter<<"\t"<<fftresult<<endl;
 
         //change temp_y to y, which is a real number version of temp_y
-        get_real<<<size_x, size_y>>>(temp_y_dev, y_dev_res);
+        get_real<<<dimension, 1>>>(y_dev_start, y_dev_res);
 
         //processing image_x
-        process_arrays<<<size_x, size_y>>>(mask_dev, y_dev_res, image_x_device, image_x_p_device, beta, int_mode, iter);
+        process_arrays<<<dimension, 1>>>(mask_dev, y_dev_res, image_x_device, image_x_p_device, beta, int_mode, iter);
 
         //fourier transform---------------------------------------------------------------------------------------------------------
         //convert real to complex (using 0 as imaginary)
-        get_complex<<<size_x, size_y>>>(image_x_device, image_x_dev_comp);
+        get_complex<<<dimension, 1>>>(image_x_device, image_x_dev_comp);
 
-        cufftExecZ2Z(plan, image_x_dev_comp, image_x_dev_res, CUFFT_FORWARD);
+        fftresult = cufftExecZ2Z(plan2d, image_x_dev_comp, image_x_dev_comp, CUFFT_FORWARD);
+        if(fftresult != CUFFT_SUCCESS) cout<<iter<<"\t"<<fftresult<<endl;
 
-        CUDA_CHECK(cudaMemcpy(x_hat, image_x_dev_res, dimension * sizeof(cufftDoubleComplex), cudaMemcpyDeviceToHost));
+        CUDA_CHECK(cudaMemcpy(x_hat, image_x_dev_comp, dimension * sizeof(cufftDoubleComplex), cudaMemcpyDeviceToHost));
 
         //this is not natively support in CUDA, still finding solution
         for(int i = 0; i < dimension; i++) 
         {
-            y_hat[i] = ptrMag[i]*exp(1i*arg(x_hat[i]));
+            y_hat[i] = ptrMag[i]*exp(complex1i*arg(x_hat[i]));
         }
     }
 
@@ -181,13 +200,11 @@ py::array_t<double> fienup_phase_retrieval(py::array_t<double> mag, int steps, b
     
     cufftDestroy(plan);
     cudaFree(y_dev_start);
-    cudaFree(temp_y_dev);
     cudaFree(y_dev_res);
     cudaFree(mask_dev);
     cudaFree(image_x_device); 
     cudaFree(image_x_p_device);
     cudaFree(image_x_dev_comp);
-    cudaFree(image_x_dev_res);
 
     auto end = chrono::high_resolution_clock::now();
     auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
@@ -198,10 +215,54 @@ py::array_t<double> fienup_phase_retrieval(py::array_t<double> mag, int steps, b
     delete[] mask;
     delete[] image_x_p;
     delete[] y_hat;
-    delete[] temp_y;
-    delete[] y;
 
     py::array_t<double> image_x_2d =  py::array(dimension, image_x);
-    image_x_2d.resize({X,Y});
+    image_x_2d.resize({Y,X});
     return image_x_2d;
+}
+
+
+//this is works without any invalids, but I dont know why this is invalid in phase retrieval function
+void test_fft(py::array_t<double> mag)
+{
+    py::buffer_info bufMag = mag.request();
+    double *ptrMag = (double *) bufMag.ptr; //magnitude 1D
+    size_t X = bufMag.shape[1]; //width of magnitude
+    size_t Y = bufMag.shape[0]; //height of magnitude
+    int size_x = static_cast<int>(X); 
+    int size_y = static_cast<int>(Y);
+    int dimension = size_x*size_y;
+    complex<double> *ptrMag2 = new complex<double>[dimension];
+
+    double *mag_dev;
+    CUDA_CHECK(cudaMalloc((void **) &mag_dev, dimension * sizeof(double)));
+    CUDA_CHECK(cudaMemcpy(mag_dev, ptrMag, dimension * sizeof(double), cudaMemcpyHostToDevice));
+
+    cufftDoubleComplex *mag_dev2;
+    CUDA_CHECK(cudaMalloc((void **) &mag_dev2, dimension * sizeof(cufftDoubleComplex)));
+
+    get_complex<<<dimension, 1>>>(mag_dev, mag_dev2);
+
+    for(int i = 0; i < 10; i++)
+    {
+        cufftHandle plan;
+        cufftResult fftresult;
+        fftresult = cufftPlan2d(&plan, dimension, dimension, CUFFT_Z2Z); //this is not invalid
+        if(fftresult != CUFFT_SUCCESS) cout<<fftresult<<endl;
+        fftresult = cufftExecZ2Z(plan, mag_dev2, mag_dev2, CUFFT_INVERSE);
+        if(fftresult != CUFFT_SUCCESS) cout<<fftresult<<endl;
+        fftresult = cufftExecZ2Z(plan, mag_dev2, mag_dev2, CUFFT_FORWARD);
+        if(fftresult != CUFFT_SUCCESS) cout<<fftresult<<endl;
+    }
+
+    CUDA_CHECK(cudaMemcpy(ptrMag2, mag_dev2, dimension * sizeof(complex<double>), cudaMemcpyDeviceToHost));
+
+    for(int i = 0; i < dimension; i++) 
+    {
+        cout<<ptrMag[i]<<"\t"<<ptrMag2[i]<<endl;
+    }
+
+    // get_real<<<size_y, size_x>>>(temp_y_dev, y_dev_res);
+
+
 }
