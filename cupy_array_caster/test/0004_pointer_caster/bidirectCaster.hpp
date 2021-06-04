4986fb7940610c783f5f241534ade78ded6e395d
afif-ishamsyah <afifhan95@gmail.com>
2021-06-04 13:08:37 +0000

    test 0003 passed, still having difficulties with test 0004

--- cupy_array_caster/test/0004_pointer_caster/bidirectCaster.hpp
+++ cupy_array_caster/test/0004_pointer_caster/bidirectCaster.hpp
@@ -1,16 +1,17 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "bidirect.hpp"
+
+using namespace std;
 
 namespace pybind11 { namespace detail {
-    template <> struct type_caster<data_container> {
+    template <> struct type_caster<simple_ptr> {
     public:
         /**
          * This macro establishes the name 'data_container' in
          * function signatures and declares a local variable
          * 'value' of type data_container
          */
-        PYBIND11_TYPE_CASTER(data_container, _("bidirect.data_container"));
+        PYBIND11_TYPE_CASTER(simple_ptr, _("numpy.ndarray"));
 
         /**
          * Conversion part 1 (Python->C++): convert a PyObject into a data_container
@@ -19,13 +20,16 @@ namespace pybind11 { namespace detail {
          */
         bool load(handle src, bool) {
 	        // TODO: implement
-            if(!hasattr(src, "data") && !hasattr(src, "scalar"))
+            if(!hasattr(src, "__array_interface__"))
             {
                  return false;
             }
             
-            value.data = src.attr("data").cast<std::vector<double>>();
-            value.scalar = src.attr("scalar").cast<double>();
+            // auto address = src.attr("__array_interface__").operator[]("data").operator[](0);
+            auto dict = src.attr("__array_interface__");
+            auto address = dict.operator[]("data").operator[](0);
+            auto hex = pybind11::module::import("builtins").attr("hex")(address);
+            *value.ptr = hex.cast<int>();
            
             return true;
         }
@@ -37,15 +41,16 @@ namespace pybind11 { namespace detail {
          * ``return_value_policy::reference_internal``) and are generally
          * ignored by implicit casters.
          */
-        static handle cast(data_container src, return_value_policy /* policy */, handle /* parent */) {
-            // TODO: implement the correct cast
-            // The return value PyLong_FromLong is only used, that it compiles
+        // static handle cast(simple_ptr src, return_value_policy /* policy */, handle /* parent */) {
+        //     // TODO: implement the correct cast
+        //     // The return value PyLong_FromLong is only used, that it compiles
 
-            auto data_container = module::import("bidirect").attr("data_container")(src.data, src.scalar);
-            return data_container.release();
+        //     // auto ndarray = module::import("numpy").attr("arrange")(1);
+        //     // ndarray.attr("__array_interface__['data']")[0] = src.ptr;
+        //     // return ndarray.release();
 
-            //return PyLong_FromLong(1);
+        //     return PyLong_FromLong(1);
 
-        }
+        // }
     };
 }} // namespace pybind11::detail
--- cupy_array_caster/test/0004_pointer_caster/pointerCaster.cpp
+++ cupy_array_caster/test/0004_pointer_caster/pointerCaster.cpp
@@ -2,24 +2,38 @@
 #include <pybind11/numpy.h>
 
 #include "ptr_container.hpp"
+#include "bidirectCaster.hpp"
 
 #include <iostream>
 #include <sstream>
 
 namespace py = pybind11;
 
+// /* Set the first element of a numpy array to 42. */
+// void change_first_element_numpy_array(py::array_t<int> &input){
+//   py::buffer_info buf = input.request();
+//   static_cast<int*>(buf.ptr)[0] = 42;
+// }
+
+// /* Returns the memory address of the numpy array data as string. */
+// std::string return_numpy_ptr_as_string(py::array_t<int> &input){
+//   py::buffer_info buf = input.request();
+
+//   std::ostringstream oss;
+//   oss << buf.ptr;
+
+//   return oss.str();
+// }
+
 /* Set the first element of a numpy array to 42. */
-void change_first_element_numpy_array(py::array_t<int> &input){
-  py::buffer_info buf = input.request();
-  static_cast<int*>(buf.ptr)[0] = 42;
+void change_first_element_numpy_array(simple_ptr input){
+  static_cast<int*>(input.ptr)[0] = 42;
 }
 
 /* Returns the memory address of the numpy array data as string. */
-std::string return_numpy_ptr_as_string(py::array_t<int> &input){
-  py::buffer_info buf = input.request();
-
+std::string return_numpy_ptr_as_string(simple_ptr input){
   std::ostringstream oss;
-  oss << buf.ptr;
+  oss << input.ptr;
 
   return oss.str();
 }
