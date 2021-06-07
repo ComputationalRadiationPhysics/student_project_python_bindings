4230e50737b67ce785b22be1c8de1a4cde656446
Simeon Ehrig <s.ehrig@hzdr.de>
2021-06-07 16:01:43 +0000

    revert Afif's changes of 0004_pointer_caster

--- cupy_array_caster/test/0004_pointer_caster/pointerCaster.cpp
+++ cupy_array_caster/test/0004_pointer_caster/pointerCaster.cpp
@@ -2,38 +2,24 @@
 #include <pybind11/numpy.h>
 
 #include "ptr_container.hpp"
-#include "bidirectCaster.hpp"
 
 #include <iostream>
 #include <sstream>
 
 namespace py = pybind11;
 
-// /* Set the first element of a numpy array to 42. */
-// void change_first_element_numpy_array(py::array_t<int> &input){
-//   py::buffer_info buf = input.request();
-//   static_cast<int*>(buf.ptr)[0] = 42;
-// }
-
-// /* Returns the memory address of the numpy array data as string. */
-// std::string return_numpy_ptr_as_string(py::array_t<int> &input){
-//   py::buffer_info buf = input.request();
-
-//   std::ostringstream oss;
-//   oss << buf.ptr;
-
-//   return oss.str();
-// }
-
 /* Set the first element of a numpy array to 42. */
-void change_first_element_numpy_array(simple_ptr input){
-  static_cast<int*>(input.ptr)[0] = 42;
+void change_first_element_numpy_array(py::array_t<int> &input){
+  py::buffer_info buf = input.request();
+  static_cast<int*>(buf.ptr)[0] = 42;
 }
 
 /* Returns the memory address of the numpy array data as string. */
-std::string return_numpy_ptr_as_string(simple_ptr input){
+std::string return_numpy_ptr_as_string(py::array_t<int> &input){
+  py::buffer_info buf = input.request();
+
   std::ostringstream oss;
-  oss << input.ptr;
+  oss << buf.ptr;
 
   return oss.str();
 }
