59a173a5c40f377e1d8b4149f7fe78c690935fac
afif-ishamsyah <afifhan95@gmail.com>
2021-06-14 15:00:13 +0000

    Delete test 10 and 11. Change most test value to be not a repeated array. New problem : Test 8 and 9 are now fail, and the cause is unknown

--- gpu_memory_management/test/test.hpp
+++ gpu_memory_management/test/test.hpp
@@ -17,55 +17,51 @@ namespace py = pybind11;
 
 __global__ void gpu_increment_all_data_by_1(double *gpu_data)
 {
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    gpu_data[i] = gpu_data[i] + 1; 
+    int i = blockIdx.x;
+    gpu_data[i] = gpu_data[i] + 1.0; 
 }
 
 //test if a pointer is a device/gpu pointer or not
-int is_device_pointer(const void *ptr)
+bool is_device_pointer(const void *ptr)
 {
-  int is_device_ptr = 0;
   cudaPointerAttributes attributes;
 
   CUDA_CHECK(cudaPointerGetAttributes(&attributes, ptr));
 
-  if(attributes.type == 2) //according to documentation, if cudaMemoryTypeDevice = 2, then it is a device memory
-                           //https://docs.nvidia.com/cuda/cuda-runtime-api/structcudaPointerAttributes.html#structcudaPointerAttributes
-                           //https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__TYPES.html#group__CUDART__TYPES_1g13de56a8fe75569530ecc3a3106e9b6d
-  {
-    is_device_ptr = 1;
-  }
+  return attributes.type == 2; 
+  //according to documentation, if cudaMemoryTypeDevice = 2, then it is a device memory
+  //https://docs.nvidia.com/cuda/cuda-runtime-api/structcudaPointerAttributes.html#structcudaPointerAttributes
+  //https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__TYPES.html#group__CUDART__TYPES_1g13de56a8fe75569530ecc3a3106e9b6d
 
-  return is_device_ptr;
 }
 
 //test 3 : test if reinterpret cast with real cupy and custom cupy will result a same value
-uint32_t test_if_reinterpret_ptr_is_the_same(size_t a_address, Custom_Cupy_Ref b)
+bool test_if_reinterpret_ptr_is_the_same(size_t a_address, Custom_Cupy_Ref b)
 {
     double * ptr = reinterpret_cast<double *>(a_address);
-    if(b.ptr == ptr) return 1;
-    else return 0;
+    if(b.ptr == ptr) return true;
+    else return false;
 }
 
 //test 4 : test if the result of the  reinterpret cast of a real cupy pointer is a device pointer
-int test_if_real_cupy_reinterpret_ptr_is_a_gpu_array(size_t a_address)
+bool test_if_real_cupy_reinterpret_ptr_is_a_gpu_array(size_t a_address)
 {
     double * ptr = reinterpret_cast<double *>(a_address);
-    int res = is_device_pointer(ptr);
+    bool res = is_device_pointer(ptr);
     
     return res;
 }
 
 //test 5 : test if the pointer of a custom cupy pointer attributes is a device pointer
-int test_if_custom_cupy_reinterpret_ptr_is_a_gpu_array(Custom_Cupy_Ref b)
+bool test_if_custom_cupy_reinterpret_ptr_is_a_gpu_array(Custom_Cupy_Ref b)
 {
-    int res = is_device_pointer(b.ptr);
+    bool res = is_device_pointer(b.ptr);
     
     return res;
 }
 
 //test 6 : copy array of float from real cupy to cpu
-uint32_t test_copy_real_cupy_to_cpu(size_t a_address, size_t a_size)
+bool test_copy_real_cupy_to_cpu(size_t a_address, size_t a_size)
 {
     double *gpu_data = reinterpret_cast<double *>(a_address);
 
@@ -73,45 +69,21 @@ uint32_t test_copy_real_cupy_to_cpu(size_t a_address, size_t a_size)
 
     CUDA_CHECK(cudaMemcpy(cpu_data.data(), gpu_data, a_size*sizeof(double), cudaMemcpyDeviceToHost));
 
-    uint32_t test = 1;
-
-    cout<<endl;
-    for(uint32_t i = 0; i < a_size; i++)
-    {
-        if(cpu_data[i] != 3.14) 
-        {
-            test = 0;
-            cout<<cpu_data[i]<<endl;
-        }
-    }
-
-    return test;
+    return (cpu_data[0] == 3.14 && cpu_data[1] == 4.25 && cpu_data[2] == 5.36);
 }
 
 //test 7 : copy array of float from custom cupy to cpu
-uint32_t test_copy_custom_cupy_to_cpu(Custom_Cupy_Ref b)
+bool test_copy_custom_cupy_to_cpu(Custom_Cupy_Ref b)
 {
     vector<double> cpu_data(b.size);
 
     CUDA_CHECK(cudaMemcpy(cpu_data.data(), b.ptr, b.size*sizeof(double), cudaMemcpyDeviceToHost));
 
-    uint32_t test = 1;
-
-    cout<<endl;
-    for(uint32_t i = 0; i < b.size; i++)
-    {
-        if(cpu_data[i] != 3.14) 
-        {
-            test = 0;
-            cout<<cpu_data[i]<<endl;
-        }
-    }
-
-    return test;
+    return (cpu_data[0] == 3.14 && cpu_data[1] == 4.25 && cpu_data[2] == 5.36);
 }
 
 //test 8 : increment all real cupy data by 1, and return the sum of all data
-double real_cupy_increment_all_data_by_1(size_t a_address, size_t a_size) 
+bool real_cupy_increment_all_data_by_1(size_t a_address, size_t a_size) 
 {
     vector<double> cpu_data(a_size);
 
@@ -119,40 +91,33 @@ double real_cupy_increment_all_data_by_1(size_t a_address, size_t a_size)
     
     gpu_increment_all_data_by_1<<<a_size, 1>>>(gpu_data);
 
-    CUDA_CHECK(cudaDeviceSynchronize());
-
     CUDA_CHECK(cudaMemcpy(cpu_data.data(), gpu_data, a_size*sizeof(double), cudaMemcpyDeviceToHost));
 
-    double sum = 0;
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-    for(uint32_t i = 0; i <  a_size; i++)
-    {
-        sum += cpu_data[i];
-    }
-
-    return sum;
+    cout<<endl;
+    cout<<cpu_data[0]<<endl;
+    cout<<cpu_data[1]<<endl;
+    cout<<cpu_data[2]<<endl;
+    
+    return (cpu_data[0] == 4.14 && cpu_data[1] == 5.25 && cpu_data[2] == 6.36);
 }
 
-//test 8 : increment all custom cupy data by 1, and return the sum of all data
-double custom_cupy_increment_all_data_by_1(Custom_Cupy_Ref b) 
+//test 9 : increment all custom cupy data by 1, and return the sum of all data
+bool custom_cupy_increment_all_data_by_1(Custom_Cupy_Ref b) 
 {
     vector<double> cpu_data(b.size);
     
     gpu_increment_all_data_by_1<<<b.size, 1>>>(b.ptr);
 
-    CUDA_CHECK(cudaDeviceSynchronize());
-
     CUDA_CHECK(cudaMemcpy(cpu_data.data(), b.ptr, b.size*sizeof(double), cudaMemcpyDeviceToHost));
 
-    double sum = 0;
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-    for(uint32_t i = 0; i <  b.size; i++)
-    {
-        sum += cpu_data[i];
-    }
-
-    return sum;
+    cout<<endl;
+    cout<<cpu_data[0]<<endl;
+    cout<<cpu_data[1]<<endl;
+    cout<<cpu_data[2]<<endl;
+    
+    return (cpu_data[0] == 4.14 && cpu_data[1] == 5.25 && cpu_data[2] == 6.36);
 }
-
-
-//note: create test with integer
--- gpu_memory_management/test/test_custom_cupy.py
+++ gpu_memory_management/test/test_custom_cupy.py
@@ -1,4 +1,3 @@
-import pytest
 import cupy as cp
 import numpy as np
 
@@ -8,7 +7,7 @@ import cupy_ref
 #test 1 : there is two way to get the cupy pointer, but are they give a same result?
 def test_two_version_for_getting_cupy_pointer():
     cp.cuda.Device(0).use()
-    a = cp.array([3.14, 3.14, 3.14])
+    a = cp.array([3.14, 4.25, 5.36])
 
     assert(a.data.ptr == a.__cuda_array_interface__["data"][0])
 
@@ -23,74 +22,57 @@ def test_getting_cupy_size():
 #test 3 : test if reinterpret cast with real cupy and custom cupy will result a same value
 def test_if_reinterpret_ptr_is_the_same():
     cp.cuda.Device(0).use()
-    a = cp.array([3.14, 3.14, 3.14])
+    a = cp.array([3.14, 4.25, 5.36])
     b = cupy_ref.Custom_Cupy_Ref(ptr = a.data.ptr, size = a.size)
     c = gpuMemManagement.test_if_reinterpret_ptr_is_the_same(a.data.ptr, b)
 
-    assert(c == 1)
+    assert(c == True)
 
 #test 4 : test if the result of the  reinterpret cast of a real cupy pointer is a device pointer
 def test_if_real_cupy_reinterpret_ptr_is_a_gpu_array():
     cp.cuda.Device(0).use()
-    a = cp.array([3.14, 3.14, 3.14])
+    a = cp.array([3.14, 4.25, 5.36])
     res = gpuMemManagement.test_if_real_cupy_reinterpret_ptr_is_a_gpu_array(a.data.ptr)
 
-    assert(res == 1)
+    assert(res == True)
 
 #test 5 : test if the pointer of a custom cupy pointer attributes is a device pointer
 def test_if_custom_cupy_reinterpret_ptr_is_a_gpu_array():
     cp.cuda.Device(0).use()
-    a = cp.array([3.14, 3.14, 3.14])
+    a = cp.array([3.14, 4.25, 5.36])
     b = cupy_ref.Custom_Cupy_Ref(ptr = a.data.ptr, size = a.size)
     res = gpuMemManagement.test_if_custom_cupy_reinterpret_ptr_is_a_gpu_array(b)
 
-    assert(res == 1)
+    assert(res == True)
 
 #test 6 : copy array of float from real cupy to cpu
 def test_copy_real_cupy_to_cpu():
     cp.cuda.Device(0).use()
-    a = cp.array([3.14, 3.14, 3.14])
+    a = cp.array([3.14, 4.25, 5.36])
     b = gpuMemManagement.test_copy_real_cupy_to_cpu(a.data.ptr, a.size)
 
-    assert(b == 1)
+    assert(b == True)
 
 #test 7 : copy array of float from custom cupy to cpu
 def test_copy_custom_cupy_to_cpu():
     cp.cuda.Device(0).use()
-    a = cp.array([3.14, 3.14, 3.14])
+    a = cp.array([3.14, 4.25, 5.36])
     b = cupy_ref.Custom_Cupy_Ref(ptr = a.data.ptr, size = a.size)
     c = gpuMemManagement.test_copy_custom_cupy_to_cpu(b)
 
-    assert(c == 1)
+    assert(c == True)
 
 #test 8 : test if real cupy can run successfully in cuda kernel
 def test_real_cupy_pointer_with_cuda_kernel():
-    a = cp.array([4.5, 4.5, 4.5])
+    a = cp.array([3.14, 4.25, 5.36])
     b = gpuMemManagement.real_cupy_increment_all_data_by_1(a.data.ptr, a.size)
 
-    assert(b == 3*(4.5 + 1))
+    assert(b == True)
 
 #test 9 : test if custom cupy can run successfully in cuda kernel
 def test_custom_cupy_pointer_with_cuda_kernel():
-    a = cp.array([4.5, 4.5, 4.5])
+    a = cp.array([3.14, 4.25, 5.36])
     b = cupy_ref.Custom_Cupy_Ref(ptr = a.data.ptr, size = a.size)
     c = gpuMemManagement.custom_cupy_increment_all_data_by_1(b)
 
-    assert(c == 3*(4.5 + 1))
-
-#one of the reason why test 1 - 9 was successfull is because all cupy array value in the test is always double precision python float,
-#the reinterpret cast of the real cupy is always casted to double, and the custom cupy current design is only accepting double type.
-#test 10 : what if instead of python float, array of integer is sent by real cupy. expectation : failure
-def test_reinterpret_integer_cupy_to_double_with_real_cupy():
-    a = cp.array([4, 4, 4, 4])
-    b = gpuMemManagement.test_copy_real_cupy_to_cpu(a.data.ptr, a.size)
-
-    assert(b == 0) #fail
-
-#test 11 : what if instead of python float, array of integer is sent by real cupy to custom cupy. expectation : failure
-def test_reinterpret_integer_cupy_to_double_with_custom_cupy():
-    a = cp.array([4, 4, 4, 4])
-    b = cupy_ref.Custom_Cupy_Ref(ptr = a.data.ptr, size = a.size)
-    c = gpuMemManagement.test_copy_custom_cupy_to_cpu(b)
-
-    assert(c == 0) #fail
+    assert(c == True)
