b64f294f040e86d84abb042bc50bb0c2d56731af
afif-ishamsyah <afifhan95@gmail.com>
2021-12-16 15:50:34 +0000

    bring back combined sync_open read and write

--- memory_reference/include/sync_open.py
+++ memory_reference/include/sync_open.py
@@ -1,6 +1,15 @@
 from contextlib import contextmanager
 
 @contextmanager
+def sync_open_rw(buffer, is_synced):
+    if is_synced:
+        buffer.read()
+        yield buffer.buffer
+        buffer.write()
+    else:
+        yield buffer
+    
+@contextmanager
 def sync_open_r(buffer, is_synced):
     if is_synced:
         buffer.read()
