e715f2876ca8baabfda5788a6475f106fa0bd38a
afif-ishamsyah <afifhan95@gmail.com>
2021-12-16 15:32:47 +0000

    delete unused comment

--- memory_reference/include/sync_open.py
+++ memory_reference/include/sync_open.py
@@ -1,14 +1,5 @@
 from contextlib import contextmanager
 
-# @contextmanager
-# def sync_open(buffer, is_synced):
-#     if is_synced:
-#         buffer.read()
-#         yield buffer.buffer
-#         buffer.write()
-#     else:
-#         yield buffer
-
 @contextmanager
 def sync_open_r(buffer, is_synced):
     if is_synced:
